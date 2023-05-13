f3224a36f29d901891bc28d96edd72b6f996595c
Jérôme Burgeat <burgeat84@hotmail.fr>
2023-05-13 12:56:34 +0200

    Feat(IPokedex) : Implémentation de la classe et test

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -13,58 +13,19 @@ class IPokedexFactoryTest {
 
     IPokemonMetadataProvider iPokemonMetadataProvider;
     IPokemonFactory iPokemonFactory;
-    IPokedex iPokedex;
     IPokedexFactory iPokedexFactory;
 
     @BeforeEach
     void initPokemon() {
 
-        iPokemonMetadataProvider = index -> null;
-        iPokemonFactory = (index, cp, hp, dust, candy) -> null;
-
-        iPokedex = new IPokedex() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public int addPokemon(Pokemon pokemon) {
-                return 0;
-            }
-
-            @Override
-            public Pokemon getPokemon(int id) throws PokedexException {
-                return null;
-            }
-
-            @Override
-            public List<Pokemon> getPokemons() {
-                return null;
-            }
-
-            @Override
-            public List<Pokemon> getPokemons(Comparator<Pokemon> order) {
-                return null;
-            }
-
-            @Override
-            public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-                return null;
-            }
-
-            @Override
-            public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
-                return null;
-            }
-        };
-
-        iPokedexFactory = (metadataProvider, pokemonFactory) -> iPokedex;
+        iPokemonMetadataProvider = new PokemonMetadataProvider();
+        iPokemonFactory = new PokemonFactory(iPokemonMetadataProvider);
+        iPokedexFactory = new  PokedexFactory();
     }
 
     @Test
     void createPokedex() {
-        assertEquals(iPokedex, iPokedexFactory.createPokedex(iPokemonMetadataProvider, iPokemonFactory));
+        assertEquals(Pokedex.class, iPokedexFactory.createPokedex(iPokemonMetadataProvider, iPokemonFactory).getClass());
     }
 
 }
