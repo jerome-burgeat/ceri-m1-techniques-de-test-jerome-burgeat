01b29e184ac72e73e8dee790be82f9bf88447fb4
Jérôme Burgeat <burgeat84@hotmail.fr>
2023-05-13 11:23:12 +0200

    Fix(IPokemonFactory) : Refactor de la classe + erreur détection Aquali

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -7,29 +7,130 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class IPokemonFactoryTest {
 
+    IPokemonMetadataProvider iPokemonMetadataProvider;
+    IPokemonFactory iPokemonFactory;
+
     Pokemon bulbizarre;
-    IPokemonFactory iBulbizarreFactory;
     Pokemon aquali;
-    IPokemonFactory iAqualieFactory;
 
     @BeforeEach
     void initPokemon() {
+        iPokemonMetadataProvider = new PokemonMetadataProvider();
+        iPokemonFactory = new PokemonFactory(iPokemonMetadataProvider);
+
         bulbizarre = new Pokemon(0,"Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
-        iBulbizarreFactory = (index, cp, hp, dust, candy) -> bulbizarre;
-
-        aquali = new Pokemon(133,"Aquali", 186, 168, 260, 2729,202,5000, 4, 100);
-        iAqualieFactory = (index, cp, hp, dust, candy) -> aquali;
-    }
-
-
-    @Test
-    void getCreateBulbizarre() throws PokedexException {
-        assertEquals(bulbizarre, iBulbizarreFactory.createPokemon(0,613, 64, 4000, 4));
+        //aquali = new Pokemon(133,"Aquali", 186, 168, 260, 2729,202,5000, 4, 100);
     }
 
     @Test
-    void getCreateAquali() throws PokedexException {
-        assertEquals(aquali, iAqualieFactory.createPokemon(133, 2729,202,5000, 4));
+    void getBulbizarreMetadata() throws PokedexException {
+        assertEquals(bulbizarre.getPokemonMetadata(bulbizarre.getIndex()),iPokemonFactory.createPokemon(0,613, 64, 4000, 4).getPokemonMetadata(bulbizarre.getIndex()));
     }
 
+    /*@Test
+    void getBulbizarreMetadata() throws PokedexException {
+        assertEquals(aquali.getPokemonMetadata(aquali.getIndex()),iPokemonFactory.createPokemon(133, 2729,202,5000, 4).getPokemonMetadata(aquali.getIndex()));
+    }*/
+
+    @Test
+    void getIndexBulbizarre() throws PokedexException {
+        assertEquals(bulbizarre.getIndex(),iPokemonFactory.createPokemon(0,613, 64, 4000, 4).getIndex());
+    }
+
+    /*@Test
+    void getIndexAquali() throws PokedexException {
+        assertEquals(aquali.getIndex(),iPokemonFactory.createPokemon(133, 2729,202,5000, 4).getIndex());
+    }*/
+
+    @Test
+    void getNameBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getName(),iPokemonFactory.createPokemon(0,613, 64, 4000, 4).getName());
+    }
+
+    /*@Test
+    void getNameAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getName(),iPokemonFactory.createPokemon(133, 2729,202,5000, 4).getName());
+    }*/
+
+    @Test
+    void getAttackBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getAttack(),iPokemonFactory.createPokemon(0,613, 64, 4000, 4).getAttack());
+    }
+
+    /*@Test
+    void getAttackAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getAttack(),iPokemonFactory.createPokemon(133, 2729,202,5000, 4).getAttack());
+    }*/
+
+    @Test
+    void getDefenseBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getDefense(),iPokemonFactory.createPokemon(0,613, 64, 4000, 4).getDefense());
+    }
+
+    /*@Test
+    void getDefenseAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getDefense(),iPokemonFactory.createPokemon(133, 2729,202,5000, 4).getDefense());
+    }*/
+
+    @Test
+    void getStaminaBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getStamina(),iPokemonFactory.createPokemon(0,613, 64, 4000, 4).getStamina());
+    }
+
+    /*@Test
+    void getStaminaAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getStamina(),iPokemonFactory.createPokemon(133, 2729,202,5000, 4).getStamina());
+    }*/
+
+    @Test
+    void getCPBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getCp(),iPokemonFactory.createPokemon(0,613, 64, 4000, 4).getCp());
+    }
+
+    /*@Test
+    void getCPAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getCp(),iPokemonFactory.createPokemon(133, 2729,202,5000, 4).getCp());
+    }*/
+
+    @Test
+    void getHPBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getHp(),iPokemonFactory.createPokemon(0,613, 64, 4000, 4).getHp());
+    }
+
+    /*@Test
+    void getHPAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getHp(),iPokemonFactory.createPokemon(133, 2729,202,5000, 4).getHp());
+    }*/
+
+    @Test
+    void getDustBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getDust(),iPokemonFactory.createPokemon(0,613, 64, 4000, 4).getDust());
+    }
+
+    /*@Test
+    void getDustAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getDust(),iPokemonFactory.createPokemon(133, 2729,202,5000, 4).getDust());
+    }*/
+
+    @Test
+    void getCandyBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getCandy(),iPokemonFactory.createPokemon(0,613, 64, 4000, 4).getCandy());
+    }
+
+    /*@Test
+    void getCandyAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getCandy(),iPokemonFactory.createPokemon(133, 2729,202,5000, 4).getCandy());
+    }*/
+
+    @Test
+    void getPokedexException() {
+        for(int i=-10; i <= -1; i++) {
+            int negatif = i;
+            assertThrows(PokedexException.class, () ->  iPokemonFactory.createPokemon(negatif,613, 64, 4000, 4).getPokemonMetadata(negatif));
+        }
+        for(int i=151; i <= 160; i++) {
+            int auDessus = i;
+            assertThrows(PokedexException.class, () ->  iPokemonFactory.createPokemon(auDessus,613, 64, 4000, 4).getPokemonMetadata(auDessus));
+        }
+    }
 }
