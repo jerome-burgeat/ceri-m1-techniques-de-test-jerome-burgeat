e71a762dd3bc8c2ccb2a939ea999247284608a59
Jérôme Burgeat <burgeat84@hotmail.fr>
2023-05-13 11:54:27 +0200

    Fix(IPokedex) : Refactor de la classe

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -11,150 +11,159 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class IPokedexTest {
 
+    IPokemonMetadataProvider iPokemonMetadataProvider;
+    IPokemonFactory iPokemonFactory;
+    List<Pokemon> pokemons;
+
+    IPokedex iPokedex;
     Pokemon bulbizarre;
-    IPokedex iPokedexBulbizarre;
     Pokemon aquali;
-    IPokedex iPokedexAquali;
 
     @BeforeEach
     void initPokemon() {
+        iPokemonMetadataProvider = new PokemonMetadataProvider();
+        iPokemonFactory = new PokemonFactory(iPokemonMetadataProvider);
+        iPokedex = new Pokedex(iPokemonMetadataProvider, iPokemonFactory);
+
         bulbizarre = new Pokemon(0,"Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
-        iPokedexBulbizarre = new IPokedex() {
-            @Override
-            public int size() {
-                return 10;
-            }
-
-            @Override
-            public int addPokemon(Pokemon pokemon) {
-                return 11;
-            }
-
-            @Override
-            public Pokemon getPokemon(int id) throws PokedexException {
-                return bulbizarre;
-            }
-
-            @Override
-            public List<Pokemon> getPokemons() {
-                List<Pokemon> listPokemons = new ArrayList<>();
-                listPokemons.add(bulbizarre);
-                listPokemons.add(aquali);
-                return listPokemons;
-            }
-
-            @Override
-            public List<Pokemon> getPokemons(Comparator<Pokemon> order) {
-                List<Pokemon> listPokemons = new ArrayList<>();
-                listPokemons.add(bulbizarre);
-                listPokemons.add(aquali);
-                return listPokemons;
-            }
-
-            @Override
-            public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-                return bulbizarre;
-            }
-
-            @Override
-            public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
-                return bulbizarre;
-            }
-        };
-
         aquali = new Pokemon(133,"Aquali", 186, 168, 260, 2729,202,5000, 4, 100);
-        iPokedexAquali = new IPokedex() {
-            @Override
-            public int size() {
-                return 2;
-            }
 
-            @Override
-            public int addPokemon(Pokemon pokemon) {
-                return 3;
-            }
-
-            @Override
-            public Pokemon getPokemon(int id) throws PokedexException {
-                return aquali;
-            }
-
-            @Override
-            public List<Pokemon> getPokemons() {
-                List<Pokemon> listPokemons = new ArrayList<>();
-                listPokemons.add(bulbizarre);
-                listPokemons.add(aquali);
-                return listPokemons;
-            }
-
-            @Override
-            public List<Pokemon> getPokemons(Comparator<Pokemon> order) {
-                List<Pokemon> listPokemons = new ArrayList<>();
-                listPokemons.add(bulbizarre);
-                listPokemons.add(aquali);
-                return listPokemons;
-            }
-
-            @Override
-            public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-                return aquali;
-            }
-
-            @Override
-            public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
-                return aquali;
-            }
-        };
-    }
-
-    @Test
-    void sizeBulbizarre() {
-        assertEquals(10, iPokedexBulbizarre.size());
-    }
-
-    @Test
-    void sizeAquali() {
-        assertEquals(2, iPokedexAquali.size());
-    }
-
-    @Test
-    void addBulbizarre() {
-        assertEquals(11, iPokedexBulbizarre.addPokemon(bulbizarre));
-    }
-
-    @Test
-    void addAquali() {
-        assertEquals(3, iPokedexAquali.addPokemon(aquali));
-    }
-
-    @Test
-    void getBulbizarre() throws PokedexException {
-        assertEquals(bulbizarre, iPokedexBulbizarre.getPokemon(0));
-    }
-
-    @Test
-    void getAquali() throws PokedexException {
-        assertEquals(aquali, iPokedexAquali.getPokemon(133));
-    }
-
-    @Test
-    void getPokemons() {
-        List<Pokemon> pokemons = new ArrayList<>();
-        pokemons.add(bulbizarre);
-        pokemons.add(aquali);
-        assertEquals(pokemons, iPokedexBulbizarre.getPokemons());
-        assertEquals(pokemons, iPokedexAquali.getPokemons());
-    }
-
-    @Test
-    void getPokemonComparator() {
-        List<Pokemon> pokemons = new ArrayList<>();
+        pokemons = new ArrayList<>();
         pokemons.add(bulbizarre);
         pokemons.add(aquali);
 
-        Comparator<Pokemon> test = (o1, o2) -> 0;
+        iPokedex.addPokemon(bulbizarre);
+        iPokedex.addPokemon(aquali);
+    }
 
-        assertEquals(pokemons, iPokedexBulbizarre.getPokemons(test));
-        assertEquals(pokemons, iPokedexAquali.getPokemons(test));
+    @Test
+    void size() {
+        assertEquals(pokemons.size(), iPokedex.getPokemons().size());
+    }
+
+    @Test
+    void getBulbizarreMetadata() throws PokedexException {
+        assertEquals(bulbizarre.getPokemonMetadata(bulbizarre.getIndex()),iPokedex.getPokemon(bulbizarre.getIndex()).getPokemonMetadata(bulbizarre.getIndex()));
+    }
+
+    /*@Test
+    void getAqualiMetadata() throws PokedexException {
+        assertEquals(aquali.getPokemonMetadata(aquali.getIndex()),iPokedex.getPokemon(aquali.getIndex()).getPokemonMetadata(aquali.getIndex()));
+    }*/
+
+    @Test
+    void getIndexBulbizarre() throws PokedexException {
+        assertEquals(bulbizarre.getIndex(),iPokedex.getPokemon(bulbizarre.getIndex()).getIndex());
+    }
+
+    /*@Test
+    void getIndexAquali() throws PokedexException {
+        assertEquals(aquali.getIndex(),iPokedex.getPokemon(aquali.getIndex()).getIndex());
+    }*/
+
+    @Test
+    void getNameBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getName(),iPokedex.getPokemon(bulbizarre.getIndex()).getName());
+    }
+
+    /*@Test
+    void getNameAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getName(),iPokedex.getPokemon(aquali.getIndex()).getName());
+    }*/
+
+    @Test
+    void getAttackBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getAttack(),iPokedex.getPokemon(bulbizarre.getIndex()).getAttack());
+    }
+
+    /*@Test
+    void getAttackAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getAttack(),iPokedex.getPokemon(aquali.getIndex()).getAttack());
+    }*/
+
+    @Test
+    void getDefenseBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getDefense(),iPokedex.getPokemon(bulbizarre.getIndex()).getDefense());
+    }
+
+    /*@Test
+    void getDefenseAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getDefense(),iPokedex.getPokemon(aquali.getIndex()).getDefense());
+    }*/
+
+    @Test
+    void getStaminaBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getStamina(),iPokedex.getPokemon(bulbizarre.getIndex()).getStamina());
+    }
+
+    /*@Test
+    void getStaminaAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getStamina(),iPokedex.getPokemon(aquali.getIndex()).getStamina());
+    }*/
+
+    @Test
+    void getCPBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getCp(),iPokedex.getPokemon(bulbizarre.getIndex()).getCp());
+    }
+
+    /*@Test
+    void getCPAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getCp(),iPokedex.getPokemon(aquali.getIndex()).getCp());
+    }*/
+
+    @Test
+    void getHPBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getHp(),iPokedex.getPokemon(bulbizarre.getIndex()).getHp());
+    }
+
+    /*@Test
+    void getHPAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getHp(),iPokedex.getPokemon(aquali.getIndex()).getHp());
+    }*/
+
+    @Test
+    void getDustBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getDust(),iPokedex.getPokemon(bulbizarre.getIndex()).getDust());
+    }
+
+    /*@Test
+    void getDustAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getDust(),iPokedex.getPokemon(aquali.getIndex()).getDust());
+    }*/
+
+    @Test
+    void getCandyBulbizarreMetadata() throws PokedexException{
+        assertEquals(bulbizarre.getCandy(),iPokedex.getPokemon(bulbizarre.getIndex()).getCandy());
+    }
+
+    /*@Test
+    void getCandyAqualiMetadata() throws PokedexException{
+        assertEquals(aquali.getCandy(),iPokedex.getPokemon(aquali.getIndex()).getCandy());
+    }*/
+
+    @Test
+    void getPokemons() throws PokedexException {
+        assertEquals(pokemons.get(0).getPokemonMetadata(bulbizarre.getIndex()), iPokedex.getPokemons().get(0).getPokemonMetadata(bulbizarre.getIndex()));
+        assertEquals(pokemons.get(1).getPokemonMetadata(aquali.getIndex()), iPokedex.getPokemons().get(1).getPokemonMetadata(aquali.getIndex()));
+    }
+
+    @Test
+    void getPokemonComparator() throws PokedexException {
+        Comparator<Pokemon> order = (o1, o2) -> 0;
+
+        assertEquals(pokemons.get(0).getPokemonMetadata(bulbizarre.getIndex()), iPokedex.getPokemons(order).get(0).getPokemonMetadata(bulbizarre.getIndex()));
+        assertEquals(pokemons.get(1).getPokemonMetadata(aquali.getIndex()), iPokedex.getPokemons(order).get(1).getPokemonMetadata(aquali.getIndex()));
+    }
+
+    @Test
+    void getPokedexException() {
+        for(int i=-10; i <= -1; i++) {
+            int negatif = i;
+            assertThrows(PokedexException.class, () -> iPokedex.getPokemon(negatif));
+        }
+        for(int i=151; i <= 160; i++) {
+            int auDessus = i;
+            assertThrows(PokedexException.class, () -> iPokedex.getPokemon(auDessus));
+        }
     }
 }
