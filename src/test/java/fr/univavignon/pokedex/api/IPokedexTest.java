7e2dd43c90ff070ccb87d85444218f6513ee4327
Jérôme Burgeat <burgeat84@hotmail.fr>
2023-05-13 12:45:49 +0200

    Fix(IPokemonTest) : Refactor de la classe

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -42,104 +42,114 @@ class IPokedexTest {
     }
 
     @Test
-    void getBulbizarreMetadata() throws PokedexException {
-        assertEquals(bulbizarre.getPokemonMetadata(bulbizarre.getIndex()),iPokedex.getPokemon(bulbizarre.getIndex()).getPokemonMetadata(bulbizarre.getIndex()));
+    void getPokemon() throws PokedexException {
+        assertEquals(Pokemon.class,iPokedex.createPokemon(0,613, 64, 4000, 4).getClass());
     }
 
-    /*@Test
+    @Test
+    void getMetadata() throws PokedexException {
+        assertEquals(Pokemon.class,iPokedex.getPokemon(bulbizarre.getIndex()).getPokemonMetadata(bulbizarre.getIndex()).getClass());
+    }
+
+    @Test
+    void getBulbizarreMetadata() throws PokedexException {
+        assertEquals(bulbizarre.getPokemonMetadata(bulbizarre.getIndex()),iPokedex.getPokemon(0).getPokemonMetadata(bulbizarre.getIndex()));
+    }
+
+    @Test
     void getAqualiMetadata() throws PokedexException {
-        assertEquals(aquali.getPokemonMetadata(aquali.getIndex()),iPokedex.getPokemon(aquali.getIndex()).getPokemonMetadata(aquali.getIndex()));
-    }*/
+        assertEquals(aquali.getPokemonMetadata(aquali.getIndex()),iPokedex.getPokemon(1).getPokemonMetadata(aquali.getIndex()));
+    }
 
     @Test
     void getIndexBulbizarre() throws PokedexException {
-        assertEquals(bulbizarre.getIndex(),iPokedex.getPokemon(bulbizarre.getIndex()).getIndex());
+        assertEquals(bulbizarre.getIndex(),iPokedex.getPokemon(0).getIndex());
     }
 
-    /*@Test
+    @Test
     void getIndexAquali() throws PokedexException {
-        assertEquals(aquali.getIndex(),iPokedex.getPokemon(aquali.getIndex()).getIndex());
-    }*/
+        assertEquals(aquali.getIndex(),iPokedex.getPokemon(1).getIndex());
+    }
 
     @Test
     void getNameBulbizarreMetadata() throws PokedexException{
-        assertEquals(bulbizarre.getName(),iPokedex.getPokemon(bulbizarre.getIndex()).getName());
+        assertEquals(bulbizarre.getName(),iPokedex.getPokemon(0).getName());
     }
 
-    /*@Test
+    @Test
     void getNameAqualiMetadata() throws PokedexException{
-        assertEquals(aquali.getName(),iPokedex.getPokemon(aquali.getIndex()).getName());
-    }*/
+        assertEquals(aquali.getName(),iPokedex.getPokemon(1).getName());
+    }
 
     @Test
     void getAttackBulbizarreMetadata() throws PokedexException{
-        assertEquals(bulbizarre.getAttack(),iPokedex.getPokemon(bulbizarre.getIndex()).getAttack());
+        assertEquals(bulbizarre.getAttack(),iPokedex.getPokemon(0).getAttack());
     }
 
-    /*@Test
+    @Test
     void getAttackAqualiMetadata() throws PokedexException{
-        assertEquals(aquali.getAttack(),iPokedex.getPokemon(aquali.getIndex()).getAttack());
-    }*/
+        assertEquals(aquali.getAttack(),iPokedex.getPokemon(1).getAttack());
+    }
 
     @Test
     void getDefenseBulbizarreMetadata() throws PokedexException{
-        assertEquals(bulbizarre.getDefense(),iPokedex.getPokemon(bulbizarre.getIndex()).getDefense());
+        assertEquals(bulbizarre.getDefense(),iPokedex.getPokemon(0).getDefense());
     }
 
-    /*@Test
+    @Test
     void getDefenseAqualiMetadata() throws PokedexException{
-        assertEquals(aquali.getDefense(),iPokedex.getPokemon(aquali.getIndex()).getDefense());
-    }*/
+        assertEquals(aquali.getDefense(),iPokedex.getPokemon(1).getDefense());
+    }
 
     @Test
     void getStaminaBulbizarreMetadata() throws PokedexException{
-        assertEquals(bulbizarre.getStamina(),iPokedex.getPokemon(bulbizarre.getIndex()).getStamina());
+        assertEquals(bulbizarre.getStamina(),iPokedex.getPokemon(0).getStamina());
     }
 
-    /*@Test
+    @Test
     void getStaminaAqualiMetadata() throws PokedexException{
-        assertEquals(aquali.getStamina(),iPokedex.getPokemon(aquali.getIndex()).getStamina());
-    }*/
+        assertEquals(aquali.getStamina(),iPokedex.getPokemon(1).getStamina());
+    }
 
     @Test
     void getCPBulbizarreMetadata() throws PokedexException{
-        assertEquals(bulbizarre.getCp(),iPokedex.getPokemon(bulbizarre.getIndex()).getCp());
+        assertEquals(bulbizarre.getCp(),iPokedex.getPokemon(0).getCp());
     }
 
-    /*@Test
+    @Test
     void getCPAqualiMetadata() throws PokedexException{
-        assertEquals(aquali.getCp(),iPokedex.getPokemon(aquali.getIndex()).getCp());
-    }*/
+        assertEquals(aquali.getCp(),iPokedex.getPokemon(1).getCp());
+    }
 
     @Test
     void getHPBulbizarreMetadata() throws PokedexException{
-        assertEquals(bulbizarre.getHp(),iPokedex.getPokemon(bulbizarre.getIndex()).getHp());
+        assertEquals(bulbizarre.getHp(),iPokedex.getPokemon(0).getHp());
     }
 
-    /*@Test
+    @Test
     void getHPAqualiMetadata() throws PokedexException{
-        assertEquals(aquali.getHp(),iPokedex.getPokemon(aquali.getIndex()).getHp());
-    }*/
+        assertEquals(aquali.getHp(),iPokedex.getPokemon(1).getHp());
+    }
 
     @Test
     void getDustBulbizarreMetadata() throws PokedexException{
-        assertEquals(bulbizarre.getDust(),iPokedex.getPokemon(bulbizarre.getIndex()).getDust());
+        assertEquals(bulbizarre.getDust(),iPokedex.getPokemon(0).getDust());
     }
 
-    /*@Test
+    @Test
     void getDustAqualiMetadata() throws PokedexException{
-        assertEquals(aquali.getDust(),iPokedex.getPokemon(aquali.getIndex()).getDust());
-    }*/
+        assertEquals(aquali.getDust(),iPokedex.getPokemon(1).getDust());
+    }
 
     @Test
     void getCandyBulbizarreMetadata() throws PokedexException{
-        assertEquals(bulbizarre.getCandy(),iPokedex.getPokemon(bulbizarre.getIndex()).getCandy());
+        assertEquals(bulbizarre.getCandy(),iPokedex.getPokemon(0).getCandy());
     }
 
-    /*@Test
+    @Test
     void getCandyAqualiMetadata() throws PokedexException{
-        assertEquals(aquali.getCandy(),iPokedex.getPokemon(aquali.getIndex()).getCandy());
-    }*/
+        assertEquals(aquali.getCandy(),iPokedex.getPokemon(1).getCandy());
+    }
 
     @Test
     void getPokemons() throws PokedexException {
